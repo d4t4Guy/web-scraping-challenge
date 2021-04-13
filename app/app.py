6dbd2163bc8e8cadb400937516d466d11afb89b7
d4t4guy <andre6180@gmail.com>
2021-04-13 16:21:29 -0700

    final submission

--- app/app.py
+++ app/app.py
@@ -1,26 +1,31 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scrape_mars
+import pymongo
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
+#conn string
+mongo_conn= 'mongodb://localhost:27017'
+client = pymongo.MongoClient(mongo_conn)
 
-
-
+#set collection variable
+mars = client.mars_app.mars
 
 @app.route("/")
 def index():
-    scrape_dict = mongo.db.mars.find_one()
-    return render_template("index.html", scrape_dict=scrape_dict)
+    if mars.count_documents({})==0: 
+        return render_template("index0.html")  #note: I tried to put the if logic in Jinja2 but after 80 minutes of trial and error I gave up and did this instead
+    else:
+        scrape_dict = mars.find_one()
+        return render_template("index.html", scrape_dict=scrape_dict)
 
 
 @app.route("/scrape")
 def scraper():
-    mars = mongo.db.mars_app.mars
-    scrape_dict = scrape_mars.scrape()
+    scrape_dict = scrape_mars.scrape()   ####replace when test done
+    mars.drop()
     mars.insert_one(scrape_dict)
     return redirect("/", code=302)
 
